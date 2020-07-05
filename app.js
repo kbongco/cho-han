35914d4d4fe8a3cc9b1b955d82bd7ce20e3c14a4
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-07-05 11:30:44 -0400

    first!

---
+++ app.js
@@ -0,0 +1,12 @@
+//Step 1: Building dice
+//Use a function to create build dice 
+//Inside create two arrays, dice1 and dice 2 
+//create an empty array combined dice 
+//use a for loop 
+
+
+function dice() {
+  dice_1 = [1, 2, 3, 4, 5, 6]
+  dice_2 = [1, 2, 3, 4, 5, 6]
+  combined = []
+}
